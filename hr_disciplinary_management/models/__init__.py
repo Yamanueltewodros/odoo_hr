990eed7b2146eb00b05bd849a9b76d88db912571
Yamanuel Tewodros <114333171+Yamanueltewodros@users.noreply.github.com>
2026-02-20 10:09:37 +0300

    Update __init__.py

--- hr_disciplinary_management/models/__init__.py
+++ hr_disciplinary_management/models/__init__.py
@@ -1,5 +1,5 @@
-from . import disciplinary_case
 from . import offense_classification
+from . import disciplinary_case
+from . import investigation
 from . import disciplinary_action
 from . import disciplinary_appeal
-from . import investigation

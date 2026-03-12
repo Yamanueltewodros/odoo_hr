8f67ee921742b09a01b59bccb57eb9e9b9552f05
Yamanuel Tewodros <114333171+Yamanueltewodros@users.noreply.github.com>
2026-03-12 13:49:56 +0300

    Create __init__.py

---
+++ sfk_operation/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from . import course
+from . import room
+from . import term
+from . import program
+from . import permanent_schedule
+from . import session
+from . import student
+from . import attendance
